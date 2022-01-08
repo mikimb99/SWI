9944a3528af8a02b504a987887cb571eb1e6b313
Miguel Pazos <miguel@tunegociointernet.com>
2022-01-08 21:29:19 +0100

    Diseño crear partido

--- express/public/scripts/functions.js
+++ express/public/scripts/functions.js
@@ -1,3 +1,22 @@
 function clear_alerts(fun) {
 	$("#alert-zone .alert").slideUp(500).promise().done(fun);
 }
+
+
+
+
+
+//FUNCIONES PARA PARTIDOS
+
+$(document).ready(function() {
+mapboxgl.accessToken = 'pk.eyJ1IjoibXBhem9zYiIsImEiOiJja3g3dW5qMzYwYTVlMm9xY3dkNjdlMnp6In0.9yDbF6ZEr1eCpFEOvfIzQQ';
+map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+	});
+ 	mapcontrol = new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl
+	});
+	map.addControl(mapcontrol);
+});
